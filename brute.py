be78c99087fa5325643717e26864eb6390fdff08
Saad Bhatti <saadbhatti111@gmail.com>
2019-09-20 01:41:40 -0500

    implemented brute force solution

---
+++ brute.py
@@ -0,0 +1,59 @@
+import math
+import numpy as np
+from itertools import permutations
+
+
+class Brute:
+    def __init__(self, cities):
+        self.cities = cities
+        self.it = 0
+
+        bestRoute = cities[list(range(0, len(cities)))]
+        self.bestRoute = np.append(bestRoute, [bestRoute[0]], axis=0)
+        self.bestDist = self.totDist(self.bestRoute)
+
+        _permutations = list(permutations(range(0, len(cities))))
+        self.permutations = [list(p) for p in _permutations]
+        pass
+
+    def update(self):
+        retVal = None
+        if self.it < len(self.permutations):
+            # get the current permutation
+            currPerm = self.permutations[self.it]
+
+            # get the route associated with perm, and append the beginning to make it a cycle
+            currRoute = self.cities[currPerm]
+            currRoute = np.append(currRoute, [currRoute[0]], axis=0)
+
+            # if currRoute is better, then update best route
+            currDist = self.totDist(currRoute)
+            if currDist < self.bestDist:
+                self.bestRoute = currRoute
+                self.bestDist = currDist
+
+            retVal = currRoute
+
+        self.it += 1
+        return retVal
+
+    def getBestRoute(self):
+        return self.bestRoute
+    def getBestDist(self):
+        return self.bestDist
+
+
+    @staticmethod
+    def euclidDist(x1, y1, z1, x2, y2, z2):
+        return math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2 + (z2 - z1) ** 2)
+
+    def totDist(self, points):
+        total = 0
+        for i in range(0, len(points) - 1):
+            currP = points[i]
+            nextP = points[i + 1]
+            total += self.euclidDist(
+                currP[0], currP[1], currP[2],
+                nextP[0], nextP[1], nextP[2]
+            )
+        return total
--- main.py
+++ main.py
@@ -4,21 +4,32 @@ import matplotlib.pyplot as plt
 import mpl_toolkits.mplot3d.axes3d as p3
 import matplotlib.animation as animation
 
+from brute import Brute
 
-def main(numCities=10):
+
+def update(frame, ax, cities, brute):
+    currRoute = brute.update()
+    bestRoute = brute.getBestRoute()
+    bestDist = brute.getBestDist()
+
+    ax.clear()
+    ax.scatter(cities[:, 0], cities[:, 1], cities[:, 2], c='b', s=50)
+    if currRoute is not None:
+        ax.plot(currRoute[:, 0], currRoute[:, 1], currRoute[:, 2], color='magenta', linestyle='dashed')
+    ax.plot(bestRoute[:, 0], bestRoute[:, 1], bestRoute[:, 2], color='blue', linestyle='solid', linewidth=2)
+
+
+def main(numCities=10, maxDist=100):
     rd.seed()
-    cities = [[100 * rd.random(), 100 * rd.random(), 100 * rd.random()] for i in range(numCities)]
+    cities = [[maxDist * rd.random(), maxDist * rd.random(), maxDist * rd.random()] for i in range(numCities)]
     cities = np.array(cities)
+    brute = Brute(cities)
 
-    fig = plt.figure()
+    fig = plt.figure(figsize=(8, 6))
     ax = p3.Axes3D(fig)
 
-    # draw cities
-    ax.scatter(cities[:, 0], cities[:, 1], cities[:, 2], c = 'b', s = 50)
-
-    # draw routes
-    ax.plot(cities[:, 0], cities[:, 1], cities[:, 2], color='red', linestyle='dashed')
+    ani = animation.FuncAnimation(fig, update, fargs=(ax, cities, brute))
     plt.show()
 
 
-main(10)
+main(6, 1)
