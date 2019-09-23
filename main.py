b92b346b3c755dd3887ec08965edade6265a4a92
Saad Bhatti <saadbhatti111@gmail.com>
2019-09-23 13:33:07 -0500

    added user input to choose brute or ga

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+"""
+Author: Saad Bhatti
+Desc:
+The Travelling Salesman Problem is a famous problem in CS. The premise is simple, given a
+list of 'cities' find the best tour/route that visits all of them. It is in the NP-Complete
+Class of problems, meaning there is no polynomial exact solution. The best we can do is
+find optimizations instead of exact solutions. This project explores 2 algorithms to find
+a tour. The first algorithm is the naive/brute force algorithm. It first finds all the permutations
+of the routes and tries them all to find the most optimal one. This algorithm has O(N!) complexity, which
+is not ideal. The second algorithm is a Genetic Algorithm. As the name suggests, it takes inspiration
+from genetics in biology. First the algorithm selects a few tours from a 'population' of tours. It then performs
+'mutations' on those few tours and creates a new population descendant from the mutations.
+This algorithm arrives at an near optimal tour much faster than the brute force, especially if the
+number of cities is large
+"""
+
 import numpy as np
 import random as rd
 import matplotlib.pyplot as plt
@@ -17,7 +33,14 @@ def update(frame, ax, cities, Algo):
 
     ax.clear()
     ax.view_init(azim=frame*2)
-    ax.set_title("Best Squared Distance So Far: {}".format(bestDist))
+
+    name = None
+    if type(Algo) == GeneticAlgo:
+        name = "Genetic Algorithm"
+    elif type(Algo) == Brute:
+        name = "Brute Force Algorithm"
+
+    ax.set_title("{0}\nBest Squared Distance So Far: {1}".format(name, round(bestDist)))
 
     ax.scatter(cities[:, 0], cities[:, 1], cities[:, 2], c='r', s=50)
     if currRoute is not None:
@@ -30,14 +53,30 @@ def main(numCities=10, maxDist=100):
     rd.seed()
     cities = [[maxDist * rd.random(), maxDist * rd.random(), maxDist * rd.random()] for i in range(numCities)]
     cities = np.array(cities)
-    #brute = Brute(cities)
-    genetic = GeneticAlgo(cities, 200)
+
+    algo = None
+
+    prompt = "\n\nSelect an Algoritm:\n" \
+             "0: Brute Force\n" \
+             "1: Genetic Algorithm\n"
+
+    while True:
+        inp = input(prompt)
+
+        if inp is '0':
+            algo = Brute(cities)
+            break
+        elif inp is '1':
+            algo = GeneticAlgo(cities, 200)
+            break
+        else:
+            continue
 
     fig = plt.figure(figsize=(8, 6))
     ax = p3.Axes3D(fig)
 
-    ani = animation.FuncAnimation(fig, update, fargs=(ax, cities, genetic), interval=1)
+    ani = animation.FuncAnimation(fig, update, fargs=(ax, cities, algo), interval=1)
     plt.show()
 
 
-main(15, 10)
+main(10, 10)
